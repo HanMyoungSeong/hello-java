695b10803991d2c469b2227b422bd12375431bea
HanMyoungSeong <audtmd12@gmail.com>
2013-09-22 20:46:43 +0900

    second commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: hms
+ * Date: 13. 9. 22.
+ * Time: 오후 8:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+}
